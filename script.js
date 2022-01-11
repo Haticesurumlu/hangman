a9ac2fba5047c82b14daf4a7801adec52c2b7636
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-01-11 12:31:23 +0300

    add function displayWord

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const word_el=document.getElementById('word');
 
+const correctLetters=
+
 function getRandomWord(){
     const words= ["Expecto Patronum","Expelliarmus","Accio","Piertotum Locomotor","Episkey","Oculus Reparo","Stupefy","Obliviate","Lumos","Crucio","Avada Kedavra"]
     return words[Math.floor(Math.random() * words.length)];
 }
+
+
+    function displayWord() {
+        const selectedWord = getRandomWord();
+    
+        word_el.innerHTML = `
+            ${selectedWord.split('').map(letter => `
+                <div class="letter">
+                    ${correctLetters.includes(letter) ?letter : ' '}
+                </div>
+            `).join('')}
+        
+        `;
+
+        const w=word_el.innerText.replace(/\n/g, '');
+        if (w===selectedWord){
+            console.log('congratulations, you won')
+        }
+}
+
+
+displayWord()
