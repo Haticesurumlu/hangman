c3973920d31e36e5d78bc5e9ab15460c654e76f4
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-01-17 23:23:31 +0300

    Diziler

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const word_el=document.getElementById('words');
+const word_el=document.getElementById('word');
 const popup= document.getElementById('popup-container');
 const message_el=document.getElementById('success-message');
-const wrongLetters_el=document.getElementById('wrong-letters');
 
 const correctLetters = ['-'];
 const wrongLetters = [];
 const selectedWord = getRandomWord();
 
-
 function getRandomWord(){
-    const words= ["expecto-patronum","expelliarmus","accio","piertotum-locomotor","episkey","oculus-reparo","stupefy","obliviate","lumos","crucio","avada-kedavra"];
+    const words= ["expecto-patronum","muhammed"]
     return words[Math.floor(Math.random() * words.length)];
 }
 
+
+// "expelliarmus","accio","piertotum-locomotor","episkey","oculus-reparo","stupefy","obliviate","lumos","crucio",
 
     function displayWord() {
        
@@ -34,35 +34,18 @@ function getRandomWord(){
         }
 }
 
-function updateWrongLetters() {
-wrongLetters_el.innerHTML= `
-${wrongLetters.length>0 ? <h3></h3> : ''}
-${wrongLetters.map(letter=>'<span> ${letter} </span>')}
-
-`;
-}
-
 window.addEventListener('keydown',function(e){
  if(e.keyCode>=32 && e.keyCode<=250){ //this range covers only the letters on the keyboard.
   const letter=e.key;
-// console.log(e.key)
-// console.log(e.keyCode)
-
-
+console.log(e.key)
+console.log(e.keyCode)
   if(selectedWord.includes(letter)){
       if(!correctLetters.includes(letter)){
           correctLetters.push(letter);
-           displayWord();
-      } else{
-          console.log("Bu harfi girmiştiniz.");
-      } }else {
-          if(!wrongLetters.includes(letter)){
-              wrongLetters.push(letter);
-              updateWrongLetters();
-          
-      }}
-  }
 
+          displayWord();
+      }
+  }
 }
 });
 
