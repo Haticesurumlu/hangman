e586c3e1839b4a911ec22a2b40b712be2f9bcd23
Haticesurumlu <71832100+Haticesurumlu@users.noreply.github.com>
2022-01-19 17:21:41 +0300

    up

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const word_el=document.getElementById('word');
 const popup= document.getElementById('popup-container');
 const message_el=document.getElementById('success-message');
+const wrongLetters_el=document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item');
 
 const correctLetters = ['-'];
 const wrongLetters = [];
@@ -34,19 +36,55 @@ function getRandomWord(){
         }
 }
 
-window.addEventListener('keydown',function(e){
- if(e.keyCode>=32 && e.keyCode<=250){ //this range covers only the letters on the keyboard.
-  const letter=e.key;
-console.log(e.key)
-console.log(e.keyCode)
-  if(selectedWord.includes(letter)){
-      if(!correctLetters.includes(letter)){
-          correctLetters.push(letter);
+function updateWrongLetters() {
+    wrongLetters_el.innerHTML = `
+        ${wrongLetters.length>0?'<h3>Hatalı harfler</h3>':''}
+        ${wrongLetters.map(letter=> `<span>${letter}<span>`)}
+    `;
 
-          displayWord();
-      }
-  }
-}
+    
+        }
+    
+
+
+// window.addEventListener('keydown',function(e){
+//  if(e.keyCode>=32 && e.keyCode<=250){ //this range covers only the letters on the keyboard.
+//   const letter=e.key;
+
+//   if(selectedWord.includes(letter)){
+//       if(!correctLetters.includes(letter)){
+//           correctLetters.push(letter);
+
+//           displayWord();
+//       }
+//   }
+// }
+// });
+
+// displayWord()
+
+
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {        
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+                displayMessage();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+                updateWrongLetters();
+            }
+            else {
+                displayMessage();
+            }
+        }
+    }
 });
 
 displayWord()
